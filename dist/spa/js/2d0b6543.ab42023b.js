6a815e77f9602f831fe32615d6376124c76bfe20
Helker Suardi Ferraz <helker_suardi@hotmail.com>
2020-05-30 16:40:04 -0300

    web dist

---
+++ dist/spa/js/2d0b6543.ab42023b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["2d0b6543"],{"1d60":function(n,o,t){"use strict";t.r(o);var u,e,l=t("b87b"),a={created:function(){l["a"].logout()}},c=a,b=t("2877"),d=Object(b["a"])(c,u,e,!1,null,null,null);o["default"]=d.exports}}]);
